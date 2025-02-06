61261568d54b2c7b32822eb0e8c8266332da68c0
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-02-06 10:45:12 -0400

    Create variaveisEConstantes.js
    Variáveis e Constantes.

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3; 
+// (Identificadores = valor) "O simbolo de IGUAL se chama atribuição" 
+let b = 4;
+
+// LET é a nova forma de declarar variaveis
+var a = 10;
+b= 12;
+
+// Não podemos redeclarar variaveis com usando Let
+
+console.log(a,b);
+
+const c = 5;
+// c= 50; -> error
+// Sendo uma constante, não temos como mudar a variavel da constante.
+console.log(c);
