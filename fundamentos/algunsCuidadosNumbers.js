2f942322b9ad555a67dd5d2f93ca721f67fdac38
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-03-03 10:13:12 -0400

    Atulizando o curso
    Continuando estudando, entendendo melhor a linguagem JS.

---
+++ fundamentos/algunsCuidadosNumbers.js
@@ -0,0 +1,4 @@
+console.log(7/0); // Retorna o type infinito.
+console.log('10' / 2); // converte string para number para fazer o calculo
+console.log("show!" * 2); // NaN
+console.log(0.1 + 0.7); // Sem precisão
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+// Linguagem Tipagem Fraca
+
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof (qualquer));
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof(qualquer));
+
+// Evite nomes génericos e siglas
+let valor = ''; // nome génerico.
+let numero = ''; // nome génerico.
+let pqp = false; // siglas evite
