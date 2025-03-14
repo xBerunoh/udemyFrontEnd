8825f36737788b143a2270bc36fa2c85f500860f
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-03-14 16:01:36 -0400

    cuidadosNumbers2
    Algumas curiosidades sobre numbers e alguns erros que podem aparecer.

--- fundamentos/algunsCuidadosNumbers.js
+++ fundamentos/algunsCuidadosNumbers.js
@@ -1,4 +1,7 @@
 console.log(7/0); // Retorna o type infinito.
 console.log('10' / 2); // converte string para number para fazer o calculo
 console.log("show!" * 2); // NaN
-console.log(0.1 + 0.7); // Sem precisão
+console.log(0.1 + 0.7); // Sem precisão\
+
+// console.log(10.toString());
+console.log((10.325).toFixed(1));
