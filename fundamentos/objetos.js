3ebed54b64c0c13d47390942a95fb5fed376fa9f
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-16 11:34:38 -0400

    Imprimindo no terminal o objeto prod1.

--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -2,5 +2,6 @@
 // Objeto - > Forma LITERAL
 // Objeto é JS é uma coleção de chaves e valores.
 const prod1 = {} // Par de chaves para criar um objeto.
-prod1.nome = 'Celular Ultra Mega' // ESSE (nome) foi criado dinamicamente dentro do objeto
+prod1.nome = 'Celular Ultra Mega' // ESSE (nome) foi criado dinamicamente dentro do objeto, Key(identificador) = Valor
 
+console.log(prod1);
