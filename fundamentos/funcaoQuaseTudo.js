16e22ef70f1e462f18b6b7d80c999a83264b498e
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-16 17:14:42 -0400

    Quase tudo é função

---
+++ fundamentos/funcaoQuaseTudo.js
@@ -0,0 +1,20 @@
+// Função é um bloco de código nomeado, que você pode chamar quantas vezes quiser dentro do seu código.
+
+console.log(typeof Object);
+
+class Produto { }
+console.log(typeof Produto)
+
+//função sem retorno
+
+function imprimirSoma(num1, num2) {
+    console.log(num1 + num2)
+
+
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2) // NaH 
+imprimirSoma(2,10,6,5,9) // vai somar somente o num1,num2 o resto vai ignorar.
+
+
