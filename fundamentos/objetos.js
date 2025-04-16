98a08ec7bccff92ab1ea0e97efc80ae46735350e
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-16 11:35:53 -0400

    Evitar nomes com espaços.

--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -3,5 +3,6 @@
 // Objeto é JS é uma coleção de chaves e valores.
 const prod1 = {} // Par de chaves para criar um objeto.
 prod1.nome = 'Celular Ultra Mega' // ESSE (nome) foi criado dinamicamente dentro do objeto, Key(identificador) = Valor
-
+prod1.preco = 4988.90
+prod1['Desconto Legal'] = 0.40 // EVITAR ATRIBUTOS COM ESPAÇOS
 console.log(prod1);
