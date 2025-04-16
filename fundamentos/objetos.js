b62701fff3f046e075096686483ca6d92af59075
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-16 11:33:37 -0400

    Criando uma chave e colocando valor nesta chave, dentro do objeto.

--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -1,4 +1,6 @@
 // JSON -> Formato textual.
 // Objeto - > Forma LITERAL
-
+// Objeto é JS é uma coleção de chaves e valores.
 const prod1 = {} // Par de chaves para criar um objeto.
+prod1.nome = 'Celular Ultra Mega' // ESSE (nome) foi criado dinamicamente dentro do objeto
+
